362cc82cb1191611720ebd23701cd3b5c6fd20bc
Nursultanuly <dnursultanuly@gmail.com>
2022-11-09 12:26:15 +0600

    c1

---
+++ lab2.py
@@ -0,0 +1,11 @@
+a=int(input())
+#a-енгіземіз
+if a==29:
+    #егер а=29 print орындалады
+    print("Nursultanuly Daulet - 010129501004")
+elif a == 92:
+    #егер а=92 болса print орындалады
+    print("Vvedite 29")
+else:
+    #print орындалады
+    print("404 not found")
