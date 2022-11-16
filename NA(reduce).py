00e7f9afa2a2608fde194a729202bbe0bd50ecc1
NurdauletAlibay <alibay.n01@mail.ru>
2022-11-16 16:01:14 +0600

    Nurdaulet

--- NA(reduce).py
+++ NA(reduce).py
@@ -1,3 +1,20 @@
 from functools import reduce
 numbers = [1, 2, 3, 4]
 print(reduce(lambda a, b: a + b, numbers))
+
+...
+
+def Square(y):
+    return (y * y)
+
+def SumofSquares(list, n):
+    summa = 0
+    for i in range(n):
+        SquaredValue = Square(list[i])
+        summa += SquaredValue
+    return summa
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+n = len(list)
+Total = SumofSquares(list, n)
+print("квадраттарының суммасы : ", Total)
